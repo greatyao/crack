171bfabb56fd01193c0d9342b941264dd9766e7f
yaowei <yaowei@2bc7da1d-2af2-3b42-8a05-e8b06c519aef>
2014-03-18 09:08:58 +0000

    laucher添加完成状态需要修改资源状态
    git-svn-id: https://202.127.26.199:8443/svn/crack@14 2bc7da1d-2af2-3b42-8a05-e8b06c519aef

--- compclient/launcher.cpp
+++ compclient/launcher.cpp
@@ -2,6 +2,9 @@
 #include "resourceslotpool.h"
 #include "CLog.h"
 #include "algorithm_types.h"
+#include "HashKill.h"
+Crack* hashkill = new HashKill();
+
 #include <string.h>
 
 #if defined(__CYGWIN__) || defined(__linux__)
@@ -14,6 +17,7 @@ clauncher::clauncher()
 {
 	m_bStop = true;
 	m_bThreadRunning = false;
+	hashkill->RegisterCallback(ReportDone, ReportStatus);
 }
 
 clauncher::~clauncher()
@@ -24,8 +28,28 @@ clauncher::~clauncher()
 	}
 }
 
-#include "HashKill.h"
-Crack* hashkill = new HashKill();
+int clauncher::ReportDone(char* guid, bool cracked, const char* passwd)
+{
+	struct _resourceslotpool *prsp;
+	prsp = ResourcePool::Get().QueryByGuid(guid);
+	if(!prsp)	return -1;
+	
+	ResourcePool::Get().Lock();
+	
+	CLog::Log(LOG_LEVEL_ERROR, "clauncher: complete crack task\n");
+	ResourcePool::Get().SetToRecover(prsp, cracked, passwd);
+	
+	ResourcePool::Get().UnLock();
+	
+	return 0;
+}
+
+int clauncher::ReportStatus(char* guid, int progress, float speed, unsigned int remainTime)
+{
+	CLog::Log(LOG_LEVEL_NOMAL, "clauncher: Progress:%d%% Speed:%g c/s Remaing %u sec\n", 
+		progress, speed, remainTime);
+	return 0;
+}
 
 void *clauncher::Thread(void*par)//扫描线程
 {
@@ -39,10 +63,7 @@ void *clauncher::Thread(void*par)//扫描线程
 		ResourcePool::Get().Lock();
 		prsp = ResourcePool::Get().LauncherQuery(uStatus);
 		if(!uStatus)
-		{
-			CLog::Log(LOG_LEVEL_WARNING,"clauncher: found non resource\n");
 			goto next;
-		}
 		
 		//处理
 		switch(uStatus)
@@ -50,19 +71,13 @@ void *clauncher::Thread(void*par)//扫描线程
 			case RS_STATUS_AVAILABLE:
 				{
 					//提交给解密插件执行，执行完毕设置执行结果
+					crack_block* block = prsp->m_item;
 					CLog::Log(LOG_LEVEL_NOMAL,"clauncher: doing crack task\n");
-					ResourcePool::Get().SetToOccupied(prsp);
-
-					//调用解密插件执行解密任务
-					crack_block* block = new crack_block();
-					block->algo = algo_md5;
-					block->charset = charset_lalphanum;
-					block->type = bruteforce;
-					sprintf(block->guid, "%p", block);
-					strcpy(block->john, "63cb5261f4610ba648fcd5e1b72c3173");
-					block->start = 4;
-					block->end = 7;
-					hashkill->StartCrack(block, block->guid, prsp->m_worker_type == DEVICE_GPU, prsp->m_device);
+					if(hashkill->StartCrack(block, block->guid, prsp->m_worker_type == DEVICE_GPU, prsp->m_device) < 0){
+						;
+					} else{
+						ResourcePool::Get().SetToOccupied(prsp);
+					}
 				}
 				break;
 			case RS_STATUS_FAILED:
@@ -89,6 +104,7 @@ next:
 	
 	return 0;
 }
+
 void clauncher::Start(void)//开始扫描线程
 {	
 	if(m_bThreadRunning==true)
@@ -102,11 +118,6 @@ void clauncher::Start(void)//开始扫描线程
 	pthread_attr_t attr;
 	pthread_attr_init(&attr);
 	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-	
-	pthread_mutex_t running_mutex;
-	pthread_cond_t keeprunning_cv;
-	pthread_mutex_init(&running_mutex, NULL);
-	pthread_cond_init(&keeprunning_cv, NULL);
 	
 	int returnValue = pthread_create( &m_pThread, &attr, Thread, (void *)this);
 	if( returnValue != 0 )
--- compclient/launcher.h
+++ compclient/launcher.h
@@ -33,7 +33,10 @@ public:
 	static void *Thread(void *);//扫描线程
 	void Start(void);			//开始扫描线程
 	void Stop(void);			//停止扫描线程
-};
+	
+	static int ReportDone(char* guid, bool cracked, const char* passwd);
 
+	static int ReportStatus(char* guid, int progress, float speed, unsigned int remainTime);
+};
 
 #endif
