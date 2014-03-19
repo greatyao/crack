bc327118de40e72ed5f58112f708336e932ba5e0
yaowei <yaowei@2bc7da1d-2af2-3b42-8a05-e8b06c519aef>
2014-03-19 05:29:15 +0000

    加入信号处理的功能
    git-svn-id: https://202.127.26.199:8443/svn/crack@22 2bc7da1d-2af2-3b42-8a05-e8b06c519aef

--- compclient/main.cpp
+++ compclient/main.cpp
@@ -1,14 +1,41 @@
-//#include <windows.h>
 #include <stdio.h>
+#include <signal.h>
+
 #include "resourceslotpool.h"
 #include "coordinator.h"
 #include "launcher.h"
 #include "CLog.h"
 #include "Client.h"
+#include "loadfiles.h"
+#include "algorithm_types.h"
 
 #if defined(__CYGWIN__) || defined(__linux__)
 #include <unistd.h>
 #endif
+
+int exit_signal = 0;
+
+static void handle_signal(int sig)
+{
+	if(sig == SIGTERM || sig == SIGINT || sig == SIGQUIT){
+		CLog::Log(LOG_LEVEL_WARNING, "Caught signal %d, exiting\n", sig);
+		exit_signal = 1;
+	}
+}
+
+static int main_loop()
+{
+	signal(SIGTERM, handle_signal); //* 下面设置三个信号的处理方法 
+	signal(SIGINT, handle_signal); 
+	signal(SIGQUIT, handle_signal); 
+	
+	int j = 0;
+	while(!exit_signal) {
+		sleep(2);
+	}
+	
+	return exit_signal;
+}
 
 int main(int argc, char *argv[])
 {
@@ -17,6 +44,15 @@ int main(int argc, char *argv[])
 		CLog::InitLogSystem(LOG_TO_FILE, true, "sys.log");
 	else
 		CLog::InitLogSystem(LOG_TO_SCREEN, true, NULL);
+#if 0		
+	struct crack_hash hashes[32];
+	load_single_hash("63cb5261f4610ba648fcd5e1b72c3173", algo_md5, hashes);
+	printf("%s\n", hashes[0].hash);
+	
+	int n = load_hashes_file("md5.hashes", algo_md5, hashes, sizeof(hashes));
+	for(int i = 0; i < n; i++)
+		printf("%d: %s\n", i, hashes[i].hash);
+#endif	
 		
 	Client::Get().Connect("192.168.18.117", 5150);
 
@@ -33,10 +69,7 @@ int main(int argc, char *argv[])
 	//启动launcher
 	pcl->Start();
 
-	//进入命令处理状态
-	//docmd(pcomp);
-	while(1)
-		sleep(1);
+	int ret = main_loop();
 	
 	//结束，清理
 	delete pcc;
@@ -44,5 +77,5 @@ int main(int argc, char *argv[])
 
 	//关闭日志系统
 	CLog::ReleaseLogSystem();
-	return 0;
+	return ret;
 }
