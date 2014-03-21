57b42557fd5b358ef1a57f135af6efa0d2ae198d
zhangrenzhuang <zhangrenzhuang@2bc7da1d-2af2-3b42-8a05-e8b06c519aef>
2014-03-21 08:10:14 +0000

    nothing changed
    git-svn-id: https://202.127.26.199:8443/svn/crack@83 2bc7da1d-2af2-3b42-8a05-e8b06c519aef

---
+++ plugins/plugin.h
@@ -0,0 +1,116 @@
+#ifndef _PLUGIN_H_
+#define _PLUGIN_H_
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+
+struct crack_hash;
+
+//hash文件加载器，对于md5等无关文件解密的类型，直接单行读取（请不要从filename解析）
+//对于word之类的文件解密的类型，直接从文件读取
+//两种类型都只有一个hash串结果，
+typedef int (*hash_plugin_parse_hash)(char *hashline, char *filename, struct crack_hash* hash);
+
+//如果是文件类的解密返回1，否则返回0
+typedef int (*hash_plugin_is_special)();
+
+//检测hash串是否合法
+typedef int (*hash_plugin_check_valid)(struct crack_hash* hash);
+
+struct hash_support_plugins
+{
+	int algo;
+	hash_plugin_parse_hash parse;
+	hash_plugin_is_special special;
+	hash_plugin_check_valid check;
+};
+
+extern struct hash_support_plugins all_plugins[];
+
+//根据算法查找其在all_plugins的索引，不存在返回NULL
+struct hash_support_plugins* locate_by_algorithm(int algo);
+
+
+//
+int md4_parse_hash(char *hashline, char *filename, struct crack_hash* hash);
+int md4_is_special();
+int md4_check_valid(struct crack_hash* hash);
+
+//md5(其他的依次类推，请在这里做几个函数申明,然后相应的实现，并填充到all_plugins)
+int md5_parse_hash(char *hashline, char *filename, struct crack_hash* hash);
+int md5_is_special();
+int md5_check_valid(struct crack_hash* hash);
+
+//
+int md5md5_parse_hash(char *hashline, char *filename, struct crack_hash* hash);
+int md5md5_is_special();
+int md5md5_check_valid(struct crack_hash* hash);
+
+//
+int md5unix_parse_hash(char *hashline, char *filename, struct crack_hash* hash);
+int md5unix_is_special();
+int md5unix_check_valid(struct crack_hash* hash);
+
+//
+int oscommerce_parse_hash(char *hashline, char *filename, struct crack_hash* hash);
+int oscommerce_is_special();
+int oscommerce_check_valid(struct crack_hash* hash);
+
+//
+int ipb2_parse_hash(char *hashline, char *filename, struct crack_hash* hash);
+int ipb2_is_special();
+int ipb2_check_valid(struct crack_hash* hash);
+
+//
+int joomla_parse_hash(char *hashline, char *filename, struct crack_hash* hash);
+int joomla_is_special();
+int joomla_check_valid(struct crack_hash* hash);
+
+//
+int vbulletin_parse_hash(char *hashline, char *filename, struct crack_hash* hash);
+int vbulletin_is_special();
+int vbulletin_check_valid(struct crack_hash* hash);
+
+//
+int desunix_parse_hash(char *hashline, char *filename, struct crack_hash* hash);
+int desunix_is_special();
+int desunix_check_valid(struct crack_hash* hash);
+
+//
+int sha1_parse_hash(char *hashline, char *filename, struct crack_hash* hash);
+int sha1_is_special();
+int sha1_check_valid(struct crack_hash* hash);
+
+//
+int sha1sha1_parse_hash(char *hashline, char *filename, struct crack_hash* hash);
+int sha1sha1_is_special();
+int sha1sha1_check_valid(struct crack_hash* hash);
+
+//
+int sha256_parse_hash(char *hashline, char *filename, struct crack_hash* hash);
+int sha256_is_special();
+int sha256_check_valid(struct crack_hash* hash);
+
+//
+int sha512_parse_hash(char *hashline, char *filename, struct crack_hash* hash);
+int sha512_is_special();
+int sha512_check_valid(struct crack_hash* hash);
+
+//
+int mysql5_parse_hash(char *hashline, char *filename, struct crack_hash* hash);
+int mysql5_is_special();
+int mysql5_check_valid(struct crack_hash* hash);
+
+//一些常用函数
+//char* strupr(char* ioString);//*******************************mike
+char* strlow(char* ioString);
+void hex2str(char *str, char *hex, int len);
+void _to64(char *s, unsigned long v, int n);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
