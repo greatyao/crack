1c775887aecf7c6a2a8f7eff3a41df1f16fd9295
zhangrenzhuang <zhangrenzhuang@2bc7da1d-2af2-3b42-8a05-e8b06c519aef>
2014-03-21 08:12:31 +0000

    str_api common string functions used when hash parsing and detecting
    git-svn-id: https://202.127.26.199:8443/svn/crack@90 2bc7da1d-2af2-3b42-8a05-e8b06c519aef

---
+++ plugins/str_api.cpp
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <ctype.h>
+#include "str_api.h"
+#include "algorithm_types.h"
+
+int ishex(char *hashline)
+{
+	char line[HASHFILE_MAX_LINE_LENGTH];
+	snprintf(line, HASHFILE_MAX_LINE_LENGTH-1, "%s", hashline);
+
+	for (int i=0;i<strlen(line);i++) 
+		if (!isxdigit(line[i])) 
+			return 0;
+
+	return 1;
+}
+
+int isAlphaDotSlash(char *hashline)
+{
+	char line[HASHFILE_MAX_LINE_LENGTH];
+	snprintf(line, HASHFILE_MAX_LINE_LENGTH-1, "%s", hashline);
+
+	for (int i=0;i<strlen(line);i++) 
+		if (!isalnum(line[i]) && '.'!=line[i] && '/'!=line[i]) 
+			return 0;
+
+	return 1;
+}
+
+int isStartsWith(char *hashline, char *str)
+{
+	std::string line = hashline;
+	if(line.find(str)==0)
+		return 1;
+	else
+		return 0;
+}
+
+int isEndsWith(char *hashline, char *str)
+{
+	std::string line = hashline;
+	if(line.find(str)==(strlen(hashline)-strlen(str)))
+		return 1;
+	else
+		return 0;
+}
+
+int isContains(char *hashline, char *str)
+{
+	std::string line = hashline;
+	if(line.find(str)!=std::string::npos)
+		return 1;
+	else
+		return 0;
+}
+
+int isupperhex(char *hashline)
+{
+	char line[HASHFILE_MAX_LINE_LENGTH];
+	snprintf(line, HASHFILE_MAX_LINE_LENGTH-1, "%s", hashline);
+
+	for (int i=0;i<strlen(line);i++) 
+		if (isalpha(line[i]) && !isupper(line[i])) 
+			return 0;
+
+	return 1;
+}
+
+std::vector<char*> split(char *hashline, char *str)
+{
+	char *c = strtok(hashline,str);
+	std::vector<char*> v;
+
+	int i=0;
+	while(c!=NULL)
+	{
+		v.push_back(c);
+		c = strtok(NULL,str);
+	}
+	return v;
+}
