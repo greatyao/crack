52a54fc13a413567b676227e9b6503e6ed3c5a85
zhangrenzhuang <zhangrenzhuang@2bc7da1d-2af2-3b42-8a05-e8b06c519aef>
2014-03-21 09:03:07 +0000

    get ride of salt lenth detection
    git-svn-id: https://202.127.26.199:8443/svn/crack@104 2bc7da1d-2af2-3b42-8a05-e8b06c519aef

---
+++ plugins/ipb2.cpp
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+#include "plugin.h"
+#include "algorithm_types.h"
+#include "err.h"
+#include "str_api.h"
+
+int ipb2_parse_hash(char *hashline, char *filename, struct crack_hash* hash)
+{
+	if (!hashline || !hash) 
+		return ERR_INVALID_PARAM;
+
+	if (strlen(hashline)<2) 
+		return ERR_INVALID_PARAM;
+
+	char line[HASHFILE_MAX_LINE_LENGTH];
+	snprintf(line, HASHFILE_MAX_LINE_LENGTH-1, "%s", hashline);
+	if(strlen(hashline)>=33)
+	{
+		std::vector<char*> v = split(line,":");
+		if((strlen(v[0]) == 32) && ishex(v[0]))
+		{
+			strcpy(hash->hash, v[0]);
+			if((v.size() == 2))
+				strcpy(hash->salt, v[1]);
+			else
+				strcpy(hash->salt, "");
+			strcpy(hash->salt2, "");
+			return 0;
+		}
+		else
+			return 1;
+	}
+	else
+		return 1;
+}
+
+int ipb2_check_valid(struct crack_hash* hash)
+{
+	if((strlen(hash->hash) == 32) && ishex(hash->hash))
+		return 1;
+	else
+		return 0;
+}
+
+int ipb2_is_special()
+{
+	return 0;
+}
