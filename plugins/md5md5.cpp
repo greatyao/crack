d2325a5776fe777072a2c419ff82df55d7d85d19
zhangrenzhuang <zhangrenzhuang@2bc7da1d-2af2-3b42-8a05-e8b06c519aef>
2014-03-21 08:07:52 +0000

    md5md5 parse
    git-svn-id: https://202.127.26.199:8443/svn/crack@77 2bc7da1d-2af2-3b42-8a05-e8b06c519aef

---
+++ plugins/md5md5.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include "plugin.h"
+#include "algorithm_types.h"
+#include "err.h"
+#include "str_api.h"
+
+int md5md5_parse_hash(char *hashline, char *filename, struct crack_hash* hash)
+{
+    return md4_parse_hash(hashline,filename,hash);
+}
+
+int md5md5_check_valid(struct crack_hash* hash)
+{
+	return md4_check_valid(hash);
+}
+
+int md5md5_is_special()
+{
+    return 0;
+}
