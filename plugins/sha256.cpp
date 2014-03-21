546cebf52e0d7578c2c48ad8036a06b4af453e85
zhangrenzhuang <zhangrenzhuang@2bc7da1d-2af2-3b42-8a05-e8b06c519aef>
2014-03-21 08:10:53 +0000

    sha256 parse
    git-svn-id: https://202.127.26.199:8443/svn/crack@86 2bc7da1d-2af2-3b42-8a05-e8b06c519aef

---
+++ plugins/sha256.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include "plugin.h"
+#include "algorithm_types.h"
+#include "err.h"
+#include "str_api.h"
+
+int sha256_parse_hash(char *hashline, char *filename, struct crack_hash* hash)
+{
+	if (!hashline || !hash) 
+		return ERR_INVALID_PARAM;
+
+	if (strlen(hashline)<2) 
+		return ERR_INVALID_PARAM;
+
+	char line[HASHFILE_MAX_LINE_LENGTH];
+	snprintf(line, HASHFILE_MAX_LINE_LENGTH-1, "%s", hashline);
+	if((strlen(line) == 64) && ishex(line))
+	{
+		strcpy(hash->hash, line);
+		strcpy(hash->salt, "");
+		strcpy(hash->salt2, "");
+		return 0;
+	}
+	else
+		return 1;
+}
+
+int sha256_check_valid(struct crack_hash* hash)
+{
+	if((strlen(hash->hash) == 64) && ishex(hash->hash))
+		return 1;
+	else
+		return 0;
+}
+
+int sha256_is_special()
+{
+	return 0;
+}
