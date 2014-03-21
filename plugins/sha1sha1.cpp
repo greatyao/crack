e128bc3557beebf66ef43da75e3d38bcbf478c12
zhangrenzhuang <zhangrenzhuang@2bc7da1d-2af2-3b42-8a05-e8b06c519aef>
2014-03-21 08:10:42 +0000

    sha1sha1 parse
    git-svn-id: https://202.127.26.199:8443/svn/crack@85 2bc7da1d-2af2-3b42-8a05-e8b06c519aef

---
+++ plugins/sha1sha1.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include "plugin.h"
+#include "algorithm_types.h"
+#include "err.h"
+#include "str_api.h"
+
+int sha1sha1_parse_hash(char *hashline, char *filename, struct crack_hash* hash)
+{
+	return sha1_parse_hash(hashline,filename,hash);
+}
+
+int sha1sha1_check_valid(struct crack_hash* hash)
+{
+	return sha1_check_valid(hash);
+}
+
+int sha1sha1_is_special()
+{
+	return 0;
+}
