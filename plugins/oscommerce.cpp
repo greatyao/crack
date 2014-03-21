bc0673e13a9875aae43de3c01424e45567f8bbae
zhangrenzhuang <zhangrenzhuang@2bc7da1d-2af2-3b42-8a05-e8b06c519aef>
2014-03-21 08:47:17 +0000

    get ride of salt lenth detection
    git-svn-id: https://202.127.26.199:8443/svn/crack@96 2bc7da1d-2af2-3b42-8a05-e8b06c519aef

---
+++ plugins/oscommerce.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+#include "plugin.h"
+#include "algorithm_types.h"
+#include "err.h"
+#include "str_api.h"
+
+int oscommerce_parse_hash(char *hashline, char *filename, struct crack_hash* hash)
+{
+	if (!hashline || !hash) 
+		return ERR_INVALID_PARAM;
+
+	if (strlen(hashline)<2) 
+		return ERR_INVALID_PARAM;
+
+	char line[HASHFILE_MAX_LINE_LENGTH];
+	snprintf(line, HASHFILE_MAX_LINE_LENGTH-1, "%s", hashline);
+	if(strlen(hashline)>33)
+	{
+		std::vector<char*> v = split(line,":");
+		if((v.size() == 2) && (strlen(v[0]) == 32) && ishex(v[0]) && ishex(v[1]))
+		{
+			strcpy(hash->hash, v[0]);
+			strcpy(hash->salt, "");
+			strcpy(hash->salt2, "");
+			return 0;
+		}
+		else
+			return 1;
+	}
+	else
+		return 1;
+}
+
+int oscommerce_check_valid(struct crack_hash* hash)
+{
+	if((strlen(hash->hash) == 32) && ishex(hash->hash))
+		return 1;
+	else
+		return 0;
+}
+
+int oscommerce_is_special()
+{
+	return 0;
+}
