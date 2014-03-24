e4c8c2470821e660d4b1f9277e1299669aeac5e1
zhangrenzhuang <zhangrenzhuang@2bc7da1d-2af2-3b42-8a05-e8b06c519aef>
2014-03-24 04:37:41 +0000

    add sha1_recovery
    git-svn-id: https://202.127.26.199:8443/svn/crack@126 2bc7da1d-2af2-3b42-8a05-e8b06c519aef

--- plugins/sha1.cpp
+++ plugins/sha1.cpp
@@ -26,8 +26,19 @@ int sha1_parse_hash(char *hashline, char *filename, struct crack_hash* hash)
 		return 1;
 }
 
+int sha1_recovery(const struct crack_hash* hash, char* line, int size)
+{
+	if(!hash || !line || size <= 0)
+		return ERR_INVALID_PARAM;
+
+	snprintf(line, size, "%s", hash->hash);
+	return 0;
+}
+
 int sha1_check_valid(struct crack_hash* hash)
 {
+	if(!hash)
+		return ERR_INVALID_PARAM;
 	if((strlen(hash->hash) == 40) && ishex(hash->hash))
 		return 1;
 	else
