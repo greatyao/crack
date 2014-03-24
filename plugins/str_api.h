f64f43934486ec0a51fda46adb655342704d8ca4
zhangrenzhuang <zhangrenzhuang@2bc7da1d-2af2-3b42-8a05-e8b06c519aef>
2014-03-24 02:59:51 +0000

    add ifndef for header
    git-svn-id: https://202.127.26.199:8443/svn/crack@114 2bc7da1d-2af2-3b42-8a05-e8b06c519aef

--- plugins/str_api.h
+++ plugins/str_api.h
@@ -1,3 +1,6 @@
+#ifndef _STR_API_H_
+#define _STR_API_H_
+
 #include <vector>
 
 int ishex(char *hashline);
@@ -7,3 +10,5 @@ int isEndsWith(char *hashline, char *str);
 int isContains(char *hashline, char *str);
 int isupperhex(char *hashline);
 std::vector<char*> split(char *hashline, char *str);
+
+#endif
