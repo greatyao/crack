9c1d22da9fc8d6838092f71282d9e90adf0e748b
zhangrenzhuang <zhangrenzhuang@2bc7da1d-2af2-3b42-8a05-e8b06c519aef>
2014-03-25 00:53:32 +0000

    add mediawiki.cpp
    git-svn-id: https://202.127.26.199:8443/svn/crack@147 2bc7da1d-2af2-3b42-8a05-e8b06c519aef

---
+++ plugins/mediawiki.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include "plugin.h"
+#include "algorithm_types.h"
+#include "err.h"
+#include "str_api.h"
+
+int mediawiki_parse_hash(char *hashline, char *filename, struct crack_hash* hash)
+{
+	return md4_parse_hash(hashline,filename,hash);
+}
+
+int mediawiki_check_valid(struct crack_hash* hash)
+{
+	return md4_check_valid(hash);
+}
+
+int mediawiki_recovery(const struct crack_hash* hash, char* line, int size)
+{
+	return md4_recovery(hash,line,size);
+}
+
+int mediawiki_is_special()
+{
+    return 0;
+}
