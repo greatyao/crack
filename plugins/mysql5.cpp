9e441185b64dc4e1dd9be02692f5625e8079283c
zhangrenzhuang <zhangrenzhuang@2bc7da1d-2af2-3b42-8a05-e8b06c519aef>
2014-03-25 01:57:21 +0000

    update isupperhex logic base on str_api.isupperhex
    git-svn-id: https://202.127.26.199:8443/svn/crack@152 2bc7da1d-2af2-3b42-8a05-e8b06c519aef

--- plugins/mysql5.cpp
+++ plugins/mysql5.cpp
@@ -17,23 +17,10 @@ int mysql5_parse_hash(char *hashline, char *filename, struct crack_hash* hash)
 	snprintf(line, HASHFILE_MAX_LINE_LENGTH-1, "%s", hashline);
 	if(((strlen(hashline)==41) && isStartsWith(hashline,"*") && isupperhex(hashline+1)) || ((strlen(hashline)==16)&&ishex(hashline)))
 	{
-		if(ishex(hashline+1))
-		{
-			if(strlen(hashline)==41)
-			{
-				strcpy(hash->hash, hashline+1);
-			}
-			else
-			{
-				strcpy(hash->hash, hashline);
-			}
-			
-			strcpy(hash->salt, "");
-			strcpy(hash->salt2, "");
-			return 0;
-		}
-		else
-			return 1;
+		strcpy(hash->hash, hashline);
+		strcpy(hash->salt, "");
+		strcpy(hash->salt2, "");
+		return 0;
 	}
 	else
 		return 1;
@@ -43,10 +30,7 @@ int mysql5_recovery(const struct crack_hash* hash, char* line, int size)
 {
 	if(!hash || !line || size <= 0)
 		return ERR_INVALID_PARAM;
-	if(strlen(hash->hash) == 16)
-		snprintf(line, size, "%s", hash->hash);
-	else
-		snprintf(line, size, "*%s", hash->hash);
+	snprintf(line, size, "%s", hash->hash);
 	return 0;
 }
 
@@ -54,7 +38,7 @@ int mysql5_check_valid(struct crack_hash* hash)
 {
 	if(!hash)
 		return ERR_INVALID_PARAM;
-	if(((strlen(hash->hash) == 40) && ishex(hash->hash) && isupperhex(hash->hash)) || ((strlen(hash->hash) == 16) && ishex(hash->hash)))
+	if(((strlen(hash->hash) == 41) && isupperhex(hash->hash+1)) || ((strlen(hash->hash) == 16) && ishex(hash->hash)))
 		return 1;
 	else
 		return 0;
