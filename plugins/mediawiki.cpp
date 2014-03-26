4bc714abb95f73975c6c2b7415154988c3d297c8
zhangrenzhuang <zhangrenzhuang@2bc7da1d-2af2-3b42-8a05-e8b06c519aef>
2014-03-26 08:28:03 +0000

    split by ':'
    git-svn-id: https://202.127.26.199:8443/svn/crack@181 2bc7da1d-2af2-3b42-8a05-e8b06c519aef

--- plugins/mediawiki.cpp
+++ plugins/mediawiki.cpp
@@ -7,58 +7,20 @@
 
 int mediawiki_parse_hash(char *hashline, char *filename, struct crack_hash* hash)
 {
-	if (!hashline || !hash) 
-		return ERR_INVALID_PARAM;
-
-	if (strlen(hashline)<2) 
-		return ERR_INVALID_PARAM;
-
-	char line[HASHFILE_MAX_LINE_LENGTH];
-	snprintf(line, HASHFILE_MAX_LINE_LENGTH-1, "%s", hashline);
-	if(strlen(hashline)>=33)
-	{
-		std::vector<char*> v = split(line,"-");
-		if((strlen(v[0]) == 32) && ishex(v[0]))
-		{
-			strcpy(hash->hash, v[0]);
-			if(v.size() == 2)
-				strcpy(hash->salt, v[1]);
-			else
-				strcpy(hash->salt, "");
-			strcpy(hash->salt2, "");
-			return 0;
-		}
-		else
-			return 1;
-	}
-	else
-		return 1;
+	return oscommerce_parse_hash(hashline,filename,hash);
 }
 
 int mediawiki_check_valid(struct crack_hash* hash)
 {
-	if(!hash)
-		return ERR_INVALID_PARAM;
-	if((strlen(hash->hash) == 32) && ishex(hash->hash))
-		return 1;
-	else
-		return 0;
+	return oscommerce_check_valid(hash);
 }
 
 int mediawiki_recovery(const struct crack_hash* hash, char* line, int size)
 {
-	if(!hash || !line || size <= 0)
-		return ERR_INVALID_PARAM;
-
-	/*if(strcmp(hash->salt, "") == 0)
-	snprintf(line, size, "%s:", hash->hash);
-	else*/
-	snprintf(line, size, "%s-%s", hash->hash, hash->salt);
-
-	return 0;
+	return oscommerce_recovery(hash,line,size);
 }
 
 int mediawiki_is_special()
 {
-	return 0;
+    return 0;
 }
