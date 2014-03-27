86bb6f745c18174b10edccbd5902c2b2b98dc7b8
liuxin <liuxin@2bc7da1d-2af2-3b42-8a05-e8b06c519aef>
2014-03-27 00:39:50 +0000

    添加头文件 + 更改工程设置
    git-svn-id: https://202.127.26.199:8443/svn/crack@187 2bc7da1d-2af2-3b42-8a05-e8b06c519aef

---
+++ server/TestServer/TestServer/ReqPacket.h
@@ -0,0 +1,40 @@
+
+
+#ifndef _REQ_PACKET_H_
+#define _REQ_PACKET_H_
+
+
+struct task_start_req {
+
+	unsigned char guid[40];
+};
+
+
+
+struct task_stop_req {
+
+	unsigned char guid[40];
+};
+
+
+
+
+struct task_pause_req {
+
+	unsigned char guid[40];
+};
+
+
+struct task_delete_req {
+
+	unsigned char guid[40];
+};
+
+
+struct task_result_req {
+
+	unsigned char guid[40];
+};
+
+#endif
+
