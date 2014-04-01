6dcacf1d9d3be9de9d541e42cb286c042944f826
yaowei <yaowei@2bc7da1d-2af2-3b42-8a05-e8b06c519aef>
2014-04-01 03:16:21 +0000

    移到common里面
    git-svn-id: https://202.127.26.199:8443/svn/crack@242 2bc7da1d-2af2-3b42-8a05-e8b06c519aef

--- server/TestServer/TestServer/ReqPacket.h
+++ server/TestServer/TestServer/ReqPacket.h
@@ -4,23 +4,6 @@
 #define _REQ_PACKET_H_
 
 
-//登录信息
-
-struct client_login_req {
-	
-	char m_osinfo[16];	//操作系统信息
-	char m_ip[20];		//IP地址信息
-
-	char m_type;		//客户端类型,control , compute
-	
-	char m_hostname[50];//主机名称
-	unsigned char m_guid[40]; //节点guid
-	
-	unsigned int m_clientsock;
-	int m_gputhreads;
-	int m_cputhreads;			
-};
-
 //心跳信息
 struct client_keeplive_req {
 
