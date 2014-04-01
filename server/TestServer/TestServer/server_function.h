348b7bd18aee2f4b05f2857275d23e2fb116b660
yaowei <yaowei@2bc7da1d-2af2-3b42-8a05-e8b06c519aef>
2014-04-01 00:46:14 +0000

    删除server_function中无用的函数
    git-svn-id: https://202.127.26.199:8443/svn/crack@233 2bc7da1d-2af2-3b42-8a05-e8b06c519aef

--- server/TestServer/TestServer/server_function.h
+++ server/TestServer/TestServer/server_function.h
@@ -10,25 +10,6 @@
 //测试函数
 VOID ProcessClientData1(LPVOID lpParameter);
 
-INT SendServerData2(LPVOID pClient, LPBYTE pData, UINT len);
-
-INT SimpleSendData(LPVOID pClient, LPBYTE pData, UINT len,int tag);
-
-
-//实际功能
-
-VOID ProcessClientData(LPVOID lpParameter);
-
-INT SendServerData(LPVOID pClient, LPBYTE pData, UINT len);
-
-INT process_recv_data(LPVOID pClient, LPBYTE pData, UINT len,BYTE cmdTag);
-
-
-
-
-
-
-
 
 
 #endif
