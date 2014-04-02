5f425eae495d174d16750bcc0c506862b85b3c5f
songzheng <songzheng@2bc7da1d-2af2-3b42-8a05-e8b06c519aef>
2014-04-02 07:59:30 +0000

    oclhashcat加入多设备的解密调用
    git-svn-id: https://202.127.26.199:8443/svn/crack@272 2bc7da1d-2af2-3b42-8a05-e8b06c519aef

--- compclient/oclHashcat.cpp
+++ compclient/oclHashcat.cpp
@@ -105,7 +105,81 @@ int oclHashcat::SupportMultiTasks()const
 
 int oclHashcat::Launcher(const crack_block* item, bool gpu, unsigned short* deviceIds, int ndevices)
 {
-	return 0;
+	unsigned char algo = item->algo;
+	unsigned char charset = item->charset;
+	unsigned short start = item->start;
+	unsigned short end = item->end;
+	const char* hash = item->john;
+	const char* fmt;
+	int i;
+	
+	if(!gpu)
+		return ERR_INVALID_PARAM;
+
+	for(i = 0; i < SUPPORT_HASH_NUM; i++)
+	{
+		if(algo == all_support_hashes[i].algo)
+		{
+			fmt = all_support_hashes[i].params;
+			break;
+		}
+	}
+	if(i == SUPPORT_HASH_NUM)
+	{
+		//未找到指定解密类型
+		return ERR_NO_SUPPORT_ALGO;
+	}
+
+	if(charset < charset_num || charset > charset_ascii)
+	{
+		//不支持自定义类型
+		return ERR_NO_SUPPORT_CHARSET;
+	}
+	
+	if(ndevices < 1 || ndevices > 16)
+	{
+		return ERR_INVALID_PARAM;
+	}
+	
+	unsigned short platformId = deviceIds[0] >> 8;
+	unsigned short ids[16] = {0};
+	for(i = 0; i < ndevices; i++)
+		ids[i] = (deviceIds[i] & 0xff) + 1;
+
+	char cmd[4096];
+	char others[128]; 
+
+	sprintf(others, "-d ", platformId);
+	for(i = 0; i < ndevices-1; i++)
+		sprintf(others, "%s%d,", others, ids[i]);
+	sprintf(others, "%s%d", others, ids[i]);
+
+	sprintf(cmd, fmt, start, end, charsets[charset], others, item->john);
+	struct maphashtarget a;
+	a.algo = algo;
+	memcpy(a.hash,item->john,sizeof(item->john));
+	if(algo==algo_mssql_2005||algo==algo_mssql_2012)
+		{
+		int i=0;
+		char c;
+		while(a.hash[i])
+		{
+			c=a.hash[i];
+			a.hash[i]=tolower(c);
+			i++;
+		}
+	}
+	this->MapTargetHash.insert(pair<string,maphashtarget>(item->guid,a));
+	
+	int pid = this->Exec(item->guid, path, cmd, MonitorThread, true, true, false);
+	
+	if(pid > 0){
+		CLog::Log(LOG_LEVEL_NOTICE, "oclHashcat: Laucher OK [guid=%s, pid=%d]\n", item->guid, pid);
+	}else{
+		CLog::Log(LOG_LEVEL_ERROR, "oclHashcat: Laucher failed [guid=%s]\n", item->guid);
+	}
+
+	return pid;	
 }
 
 int oclHashcat::Launcher(const crack_block* item, bool gpu, unsigned short deviceId)
