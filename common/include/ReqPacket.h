4e578ea257559534688b4bdff2a8c4e486b94b2f
yaowei <yaowei@2bc7da1d-2af2-3b42-8a05-e8b06c519aef>
2014-04-04 01:08:36 +0000

    转移到公共目录
    git-svn-id: https://202.127.26.199:8443/svn/crack@303 2bc7da1d-2af2-3b42-8a05-e8b06c519aef

---
+++ common/include/ReqPacket.h
@@ -0,0 +1,60 @@
+
+
+#ifndef _REQ_PACKET_H_
+#define _REQ_PACKET_H_
+
+
+struct task_start_req {
+
+	unsigned char guid[40];
+};
+
+
+
+struct task_stop_req {
+
+	unsigned char guid[40];
+};
+
+
+
+
+struct task_pause_req {
+
+	unsigned char guid[40];
+};
+
+
+struct task_delete_req {
+
+	unsigned char guid[40];
+};
+
+
+struct task_result_req {
+
+	unsigned char guid[40];
+};
+
+
+
+struct file_upload_req {
+
+	unsigned char guid[40];
+
+};
+
+
+struct file_upload_start_req {
+
+
+	unsigned char guid[40];
+	void *f;
+	unsigned int len;
+	unsigned int offset;
+
+};
+
+
+#endif
+
