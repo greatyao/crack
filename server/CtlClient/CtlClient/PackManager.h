8c6ec54570532eee1ad397f8c808cc0128ec7d58
yaowei <yaowei@2bc7da1d-2af2-3b42-8a05-e8b06c519aef>
2014-04-11 07:35:05 +0000

    删除无用代码
    git-svn-id: https://202.127.26.199:8443/svn/crack@406 2bc7da1d-2af2-3b42-8a05-e8b06c519aef

--- server/CtlClient/CtlClient/PackManager.h
+++ server/CtlClient/CtlClient/PackManager.h
@@ -34,18 +34,9 @@ public:
 
 	int GenTaskDeletePackt(task_delete_req req,task_status_res *res);
 
-//	int GenTaskResultPack(task_result_req req,task_status_res *res);
-
 	int GenTaskStatusPack(task_status_info **res);
 
 	int GenClientStatusPack(compute_node_info **res);
-
-	
-	/*
-	int GenFileUploadPack(file_upload_req req,file_upload_res *res);
-
-	int GenFileUploadStart(file_upload_end_res *res);
-	*/
 
 	//修改获得任务结果的协议
 	int GenTaskResultPack(task_result_req req,task_result_info **res);
@@ -76,25 +67,6 @@ public:
 	char m_cur_local_file[256];
 	void * m_file_desc;
 	CSocketClient m_sockclient;
-
-	//数据发送线程
-	pthread_t m_ThreadSend;	//发送网络数据的线程句柄
-	int m_bThreadSendStop;	//停止线程标记
-	int m_bThreadSendRunning;//线程运行标记
-
-	unsigned char *m_pSend;//发送的数据 
-	int m_bToSend;//有数据发送
-	int m_lenSend;//数据长度
-
-	unsigned char *m_pRecv;//接收到数据的指针
-	int m_bRecved;//接收到数据
-	int m_lenRecv;//
-
-	static void *ThreadSend(void *);	//发送网络数据的线程
-	void StartSend(void);				//启动发送数据的线程
-	void StopSend(void);				//停止发送数据的线程
-
-	void *SendDataViaThread(void *);//发送数据接口
 
 
 	//增加线程，处理心跳包
