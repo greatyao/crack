59d50acedb77b099fcebaf68198d82139e29d1fa
yaowei <yaowei@2bc7da1d-2af2-3b42-8a05-e8b06c519aef>
2014-05-19 13:55:39 +0000

    将map中的key由char* 改成string
    git-svn-id: https://202.127.26.199:8443/svn/crack@629 2bc7da1d-2af2-3b42-8a05-e8b06c519aef

--- server/TestServer/TestServer/TestServer.h
+++ server/TestServer/TestServer/TestServer.h
@@ -20,37 +20,31 @@ struct MapLessCompare{
 		return strcmp(str1,str2) < 0;
 	}
 
+	bool operator()(char * str1, char *str2) const
+	{
+		return strcmp(str1,str2) < 0;
+	}
+
 	bool operator()(const string& str1,const string& str2) const
 	{
 		return strcmp(str1.c_str(),str2.c_str()) < 0;
 	}
 };
 
-//typedef std::map<char *, CCrackBlock *,MapLessCompare > TOTAL_CB_MAP;
-
-typedef std::vector<char *> GUID_VECTOR;
+typedef std::vector<string> GUID_VECTOR;
 
 typedef std::vector<CBlockNotice *> CBN_VECTOR;
 
 typedef std::vector<CClientInfo *> CI_VECTOR;
 
-typedef std::map<char *,CCrackTask *,MapLessCompare> CT_MAP;
+typedef std::map<string,CCrackTask *,MapLessCompare> CT_MAP;
 
 typedef std::map<string,CBN_VECTOR,MapLessCompare> CCB_MAP; //computer<----> block ”≥…‰
 
-//typedef std::queue<char *> CT_QUEUE;
 typedef std::deque<string> CT_DEQUE;
 
-struct CBMapLessCompare{
-	
-	bool operator()(const char * str1,const char *str2) const
-	{
+typedef std::map<string, CCrackBlock *, MapLessCompare> CB_MAP;
 
-		return strcmp(str1,str2) < 0;
-	}
-};
-
-typedef std::map<char *, CCrackBlock *,CBMapLessCompare> CB_MAP;
 typedef std::vector<CCrackHash *> CRACK_HASH_LIST;
 
 typedef std::map<string, CCrackTask *, MapLessCompare> CT_MAP2;
